4817bb393f4042396b99cbba91090fe341080b90
Kevin Wickham <kevin@mh212-fedora.engineering.iastate.edu>
2012-05-25 15:01:48 -0500

    cakephp

--- functions.php
+++ functions.php
@@ -1,28 +1,4 @@
 <?php
-function get_header(){
-	?>
-	<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-	<html>
-	<head>
-	<title>My page</title>
-	
-	</head>
-	<body>
-	
-	
-	
-	<?php 
-	
-}
-function get_footer(){
-	
-	?></body>
-	
-	
-	
-	</html>
-	<?php
-	
-}
+require_once 'backpress/includes/class.bpdb.php';
 
 ?>
