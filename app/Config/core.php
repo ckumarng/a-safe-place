029e381b60168a44321a3ba4aaa2b3835a643c86
Kevin Wickham <kwickham@iastate.edu>
2012-07-08 11:25:10 -0500

    updates

--- app/Config/core.php
+++ app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '3b0qyJfIDxfs2dfR54guVoDFG534r6UubWaC9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '768599657446923535424967493645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
