6ecd0235f2ac6e99a5fbb5185f65b6e62982e0b6
Kevin Wickham <kwickham@iastate.edu>
2012-07-15 18:36:46 -0500

    updates

--- app/Controller/LoginController.php
+++ app/Controller/LoginController.php
@@ -4,7 +4,7 @@ class LoginController extends AppController {
     public $helpers = array('Html', 'Form','Session');
 
 	public function index() {
-
+       // debug($this->Session->read());
                         if($this->request->is('post')){
  //print_r($this->data);
 
@@ -12,19 +12,20 @@ class LoginController extends AppController {
                 if( $this->data['Login']['ID'] != '' ){
                     //$this->Session->activate();
                     //print_r($this -> Session -> read());
-                    if ($this->Session->started()){
+                    //if ($this->Session->started()){
 
                         //echo 'cool';
+                        //$this->Session->setFlash($thi);
                         $this->Session->write ('pid',trim($this->data['Login']['ID']));
                         if ( ! $this->Session->check('activity'))
                             $this->Session->write ('activity', 1);
 
                   // print_r($this -> Session ->error());
 
-                    }
+                    //}
 
                  //if (  $this->Session->check('pid'))
-                    $this->redirect ('/study/firstStudy');
+                    $this->redirect ('/NextSection');
 
 
                 }
--- app/View/Layouts/default.ctp
+++ app/View/Layouts/default.ctp
@@ -56,6 +56,7 @@ $cakeDescription = __d('cake_dev', 'CakePHP: the rapid development php framework
 			?>
 		</div>
 	</div>
-	<?php //echo $this->element('sql_dump'); ?>
+	<?php echo $this->element('sql_dump'); ?>
+        <?php debug($this->Session->read()); ?>
 </body>
 </html>
