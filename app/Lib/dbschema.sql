c7c6507da4e0a4af7d24c4abe075c59c83ca17d2
root <root@www.semantiknit.com>
2012-07-19 23:34:57 +0000

    Modified comments in mysql db schema to import w/o complaining ALL THE TIME, about EVERY LITTLE THING, NO MATTER HOW MINOR.  CAN I HELP IT IF I JUST *FORGOT* ONE LITTLE WHITESPACE CHARACTER?  I MEAN, DID THE *WORLD* END?? DID SOMEONE **DIE** ???  JUST CREATE THE #$!@& DATABASE DAMMIT!

--- app/Lib/dbschema.sql
+++ app/Lib/dbschema.sql
@@ -1,4 +1,4 @@
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `survey` (
   `user_id` bigint(10) unsigned NOT NULL,
   `1` varchar(254) NOT NULL DEFAULT '',
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS `survey` (
   `13` varchar(254) NOT NULL DEFAULT '',
   PRIMARY KEY (`user_id`)
 ) ;
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `answers` (
   `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `module_id` bigint(10) unsigned NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `answers` (
   `correct` smallint NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
---updated: 7/19/2012 4:50
+-- updated: 7/19/2012 4:50
 CREATE TABLE IF NOT EXISTS `modules` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `module` bigint(10) unsigned NOT NULL,
@@ -39,20 +39,20 @@ CREATE TABLE IF NOT EXISTS `modules` (
   PRIMARY KEY (`id`),
   KEY `user` (`user`)
 );
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `rank` (
   `study_id` bigint(10) unsigned NOT NULL,
   `rank_array` tinytext NOT NULL,
   PRIMARY KEY (`study_id`)
 ) ;
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `robot_link` (
   `user_id` bigint(10) unsigned NOT NULL,
   `robot_rank` int(10) unsigned NOT NULL,
   `user_rank` int(10) unsigned NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ;
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `study` (
   `study_id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `date` time NOT NULL,
@@ -60,21 +60,21 @@ CREATE TABLE IF NOT EXISTS `study` (
   `participants` tinytext NOT NULL,
   PRIMARY KEY (`study_id`)
 ) AUTO_INCREMENT=1 ;
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `team_link` (
   `team_id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_1` int(10) unsigned NOT NULL,
   `user_2` int(10) unsigned NOT NULL,
   PRIMARY KEY (`team_id`)
 ) AUTO_INCREMENT=1 ;
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `random_numbers` (
   `id` int(15) unsigned NOT NULL AUTO_INCREMENT,
   `first` smallint(1) unsigned NOT NULL,
   `second` smallint(3) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 );
---updated: original
+-- updated: original
 CREATE TABLE IF NOT EXISTS `logins` (
   `id` varchar(254) NOT NULL,
   `first_name` varchar(254) NOT NULL,
